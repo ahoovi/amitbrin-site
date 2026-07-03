073590d3b5327b28d7cd46ef8296de4ba1151eee
ahoovi <49495106+ahoovi@users.noreply.github.com>
2026-07-04 01:11:23 +0300

    gorlin.help: add /en host rewrite

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
     return {
       beforeFiles: GORLIN_HOSTS.flatMap((host) => [
         { source: "/", has: [{ type: "host", value: host }], destination: "/gorlin" },
+        { source: "/en", has: [{ type: "host", value: host }], destination: "/gorlin/en" },
+        { source: "/en/:path*", has: [{ type: "host", value: host }], destination: "/gorlin/en/:path*" },
         { source: "/robots.txt", has: [{ type: "host", value: host }], destination: "/gorlin/robots.txt" },
         { source: "/sitemap.xml", has: [{ type: "host", value: host }], destination: "/gorlin/sitemap.xml" },
         { source: "/og-image.jpg", has: [{ type: "host", value: host }], destination: "/gorlin/og-image.jpg" },
